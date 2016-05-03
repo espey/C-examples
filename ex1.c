44f5c3b2acab2a7645713a562cc388604768aa09
espey <erinpolsfoot@gmail.com>
2016-05-02 22:26:13 -0500

    exercise 1 from learn C the hard way

---
+++ ex1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    puts("Two households, both alike");
+    puts("in dignity,");
+    puts("In fair Verona, where we lay our scene,");
+    puts("From ancient");
+    puts("grudge");
+    puts("b");
+    puts("  r");
+    puts("    e");
+    puts("      a");
+    puts("        k");
+    puts("To new mutiny");
+
+    return 0;
+}
